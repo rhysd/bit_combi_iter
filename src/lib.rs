df72901fb5b65860a409e84257e6f27efb5558de
rhysd <lin90162@yahoo.co.jp>
2021-09-07 23:08:41 +0900

    div never wraps

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,9 @@ macro_rules! impl_uint {
                 fn next_combination(self) -> Option<Self> {
                     let a = self;
                     let b = a ^ a.wrapping_add(1);
-                    let c = a.wrapping_sub(b.wrapping_div(2));
+                    let c = a.wrapping_sub(b / 2);
                     if let Some(d) = b.checked_add(1) {
-                        let e = c.wrapping_sub((c & c.wrapping_neg()).wrapping_div(d));
+                        let e = c.wrapping_sub((c & c.wrapping_neg()) / d);
                         if e != 0 { Some(e) } else { None }
                     } else {
                         None
